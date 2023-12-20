59027c25a07fbb35e9402b346c569e26cbb98645
shogo <shogo.ohashi0417@gmail.com>
2023-12-20 21:32:51 +0900

    fix/UI修正

--- resources/js/Pages/ToDoList.jsx
+++ resources/js/Pages/ToDoList.jsx
@@ -44,6 +44,8 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                             <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                                 2023 12/31 (Sun)
                                             </td>
+                                            <td>
+                                            </td>
                                         </tr>
                                     </tbody>
                                 </table>
