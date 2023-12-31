c0e68045f499819633890c4f5a9d90bdc9a7698e
shogo <shogo.ohashi0417@gmail.com>
2024-01-01 08:39:15 +0900

    update/todo追加モーダルの作成

--- resources/views/todo_list.blade.php
+++ resources/views/todo_list.blade.php
@@ -7,6 +7,7 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
@@ -36,7 +37,7 @@
                                             DeadLine
                                         </th>
                                         <th class='w-10'>
-                                            <FontAwesomeIcon icon={faCirclePlus} size="xl" />
+                                            <a type="button" id="addTodoList"><i class="fas fa-plus"></i></a>
                                         </th>
                                     </tr>
                                 </thead>
@@ -65,6 +66,54 @@
                     </div>
                 </div>
             </div>
+            <div id="addTodoListModal" class="fixed top-0 left-0 w-full h-full flex items-center justify-center hidden">
+                <div class="absolute w-full h-full bg-gray-900 opacity-50">
+                        
+                </div>
+                <div class="z-10 bg-white p-6 rounded shadow-lg w-1/2">
+                    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                            Create New Product
+                        </h3>
+                        <button id="closeModal" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="addTodoListModal">
+                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                            </svg>
+                            <span class="sr-only">Close modal</span>
+                        </button>
+                    </div>
+                    <form action="" method="POST" class="p-4 md:p-5">
+                        @csrf
+                        <div class="grid gap-4 mb-4 grid-cols-2">
+                            <div class="col-span-2">
+                                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Todo Name</label>
+                                <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="">
+                            </div>
+                            <div class="col-span-2">
+                                <input type="date" name="dead_line" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            </div>
+                        </div>
+                        <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+                            Add new todo
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+            <script>
+                const addTodoListButton  = document.getElementById('addTodoList');
+                const closeModalButton   = document.getElementById('closeModal');
+                const addTodoListModal = document.getElementById('addTodoListModal');
+
+                addTodoListButton.addEventListener('click', () => {
+                    addTodoListModal.classList.remove('hidden');
+                });
+
+                closeModalButton.addEventListener('click', () => {
+                addTodoListModal.classList.add('hidden');
+                });
+            </script>
             <footer class='font-bold bg-slate-800 text-white text-center p-2'>
                 フッター
             </footer>
