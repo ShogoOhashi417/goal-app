afc22cd10af003252b767bdb8beeae0a790dc653
shogo <shogo.ohashi0417@gmail.com>
2024-02-09 08:00:04 +0900

    update/タスク編集処理を追加

--- resources/js/src/Task.jsx
+++ resources/js/src/Task.jsx
@@ -9,7 +9,6 @@ import { createHeadManager } from '@inertiajs/core';
 
 const changeDate = (date) => {
     const originalDate = new Date(date);
-    const month = originalDate.getMonth() + 1;
 
     const year = originalDate.getFullYear();
     const month = (originalDate.getMonth() + 1).toString().padStart(2, '0');
@@ -74,6 +73,19 @@ function Task() {
         setTaskDeadLine('');
     }
 
+    const editTask = () => {
+        console.log(taskId)
+        axios.post('/task/update', {
+            'id' : taskId,
+            'name' : taskName,
+            'dead_line' : taskDeadLine
+        })
+        .then(response => {
+            fetchTaskList();
+            closeModal();
+        });
+    }
+
     const editTaskRef = useRef(null);
 
     const openEditTaskModal = (
@@ -89,6 +101,8 @@ function Task() {
     }
 
     const closeModal = () => {
+        setTaskName('');
+        setTaskDeadLine('');
         addTaskRef.current.classList.add('hidden');
         editTaskRef.current.classList.add('hidden');
     }
@@ -228,22 +242,36 @@ function Task() {
                             <span className="sr-only">Close modal</span>
                         </button>
                     </div>
-                    <form action="/task/update" method="POST" className="p-4 md:p-5">
-                        <div className="grid gap-4 mb-4 grid-cols-2">
-                            <div className="col-span-2">
-                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Task Name</label>
-                                <input type="text"  id="taskName" name="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="" />
-                            </div>
-                            <div className="col-span-2">
-                                <input type="date" id="deadLine" name="dead_line" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" />
-                            </div>
+                    <div className="grid gap-4 mb-4 grid-cols-2">
+                        <div className="col-span-2">
+                            <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Task Name</label>
+                            <input
+                                type="text"
+                                name="name"
+                                value={taskName}
+                                onChange={taskNameChange}
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required=""
+                            />
                         </div>
-                        <button type="submit" className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            <svg className="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd"></path></svg>
-                            Edit task
-                        </button>
-                        <input type="hidden" id="taskId" name="task_id" />
-                    </form>
+                        <div className="col-span-2">
+                            <input
+                                type="date"
+                                name="dead_line"
+                                value={convertDateFormat(changeDate(taskDeadLine))}
+                                onChange={taskDeadLineChange}
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            />
+                        </div>
+                    </div>
+                    <button
+                        onClick={editTask}
+                        type="submit"
+                        style={{ opacity: isTaskInfoFilled ? 1 : 0.5 }}
+                        disabled={!isTaskInfoFilled}
+                        className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <svg className="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd"></path></svg>
+                        Edit task
+                    </button>
                 </div>
             </div>
             
