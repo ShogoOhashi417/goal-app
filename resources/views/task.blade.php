edf4781b68500f5e7f385a54ca82407b417b7d69
shogo <shogo.ohashi0417@gmail.com>
2024-02-15 07:51:33 +0900

    update/ログインからタスク一覧への動線作成

--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -1,33 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Task') }}
+        </h2>
+    </x-slot>
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://use.fontawesome.com/releases/v6.2.0/css/all.css" rel="stylesheet">
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-        @vite(['resources/css/app.css', 'resources/js/app.jsx'])
-
-    </head>
-    <body>
-        <div class='flex flex-col min-h-screen'>
-            <header class='font-bold bg-slate-800 text-white text-center p-2'>
-                ヘッダー
-            </header>
-            <div id="task-page" data-props="{{ json_encode($task_list) }}">
-
-            </div>
-            {{-- <footer class='fixed bottom-0 font-bold bg-slate-800 text-white text-center p-2'> --}}
-            <footer class='font-bold bg-slate-800 text-white text-center p-2'>
-                フッター
-            </footer>
-        </div>
-    </body>
-</html>
+    <div id="task-page" data-props="{{ json_encode($task_list) }}">
+    </div>
+</x-app-layout>
