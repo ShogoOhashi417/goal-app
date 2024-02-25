69a4eba64166dbfbbed7191960cd239df7a14c50
shogo <shogo.ohashi0417@gmail.com>
2024-02-25 16:38:56 +0900

    update/データの取得をDBから行う

--- app/Infrastructure/LifeInsurance/LifeInsuranceRepository.php
+++ app/Infrastructure/LifeInsurance/LifeInsuranceRepository.php
@@ -6,42 +6,13 @@ namespace App\Infrastructure\LifeInsurance;
 
 use App\Domain\LifeInsurance\LifeInsuranceRepositoryInterface;
 use App\Domain\LifeInsurance\LifeInsurance;
+use App\Models\LifeInsurance as LifeInsuranceModel;
 
 class LifeInsuranceRepository implements LifeInsuranceRepositoryInterface {
     
     public function fetchAll(): array
     {
-        // todo DBから取得する
-        return [
-            [
-                'id' => 1,
-                'name' => '長生きゴールド',
-                'fee' => 1000,
-                'payment_type' => '月払い',
-                'insurance_type' => '定期'
-            ],
-            [
-                'id' => 2,
-                'name' => 'もしもの味方',
-                'fee' => 12000,
-                'payment_type' => '年払',
-                'insurance_type' => '定期'
-            ],
-            [
-                'id' => 3,
-                'name' => 'コツコツ年金',
-                'fee' => 240000,
-                'payment_type' => '年払',
-                'insurance_type' => '年金'
-            ],
-            [
-                'id' => 4,
-                'name' => '安心丸ごと自動車',
-                'fee' => 3000,
-                'payment_type' => '年払い',
-                'insurance_type' => '定期'
-            ]
-        ];
+        return LifeInsuranceModel::all()->toArray();
     }
 
     public function save(LifeInsurance $lifeInsurance)
