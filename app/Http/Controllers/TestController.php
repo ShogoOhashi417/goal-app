c6e052243325e4cb85b2c4a7e5baa8ff3a6e7277
shogo <shogo.ohashi0417@gmail.com>
2024-03-02 10:45:31 +0900

    明示的にインスタンスを生成する

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,8 +7,15 @@ use App\TestService\TestService;
 
 class TestController extends Controller
 {
-    public function index(TestService $testService)
+    public function index()
     {
+        $testService = app('App\TestService\TestService');
+        // もしくは
+        $testService = app()->make('App\TestService\TestService');
+        // もしくは
+        $testService = resolve('App\TestService\TestService');
+
+        // newすればいいのだが、シングルトンパターンだとそうもいかない
         dd($testService->say());
         return view('welcome');
     }
