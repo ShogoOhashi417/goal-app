3779996019636dd1937fc3c6ab3098069d56d361
shogo <shogo.ohashi0417@gmail.com>
2024-03-05 07:50:03 +0900

    update/保険料の計算ロジックを追加

--- app/Domain/LifeInsurance/Fee.php
+++ app/Domain/LifeInsurance/Fee.php
@@ -7,14 +7,13 @@ namespace App\Domain\LifeInsurance;
 use Exception;
 
 class Fee {
+    const SIX_MONTHS = 6;
+    const MONTHS_PER_ONE_YEAR = 12;
+
     private readonly int $fee;
 
     public function __construct(int $fee)
     {
-        if (!$fee) {
-            throw new Exception('保険料は1円以上を入力してください');
-        }
-        
         $this->fee = $fee;
     }
 
@@ -25,5 +24,31 @@ class Fee {
     public function getFee(): int
     {
         return $this->fee;
+    }
+    
+    /**
+     *
+     * @param integer $amount
+     * @param PaymentType $paymentType
+     * @return self
+     */
+    public function add(int $amount, PaymentType $paymentType): self
+    {
+        if ($paymentType->isYealy()) {
+            $added_amount = $this->fee + $amount;
+            return new Fee($added_amount);
+        }
+
+        if ($paymentType->isHalfYearly()) {
+            $added_amount = $this->fee + $amount * self::SIX_MONTHS;
+            return new Fee($added_amount);
+        }
+
+        if ($paymentType->isMonthly()) {
+            $added_amount = $this->fee + $amount * self::MONTHS_PER_ONE_YEAR;
+            return new Fee($added_amount);
+        }
+
+        return $this;
     }
 }
