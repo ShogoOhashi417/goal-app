0cea96b9dda21e772bacdbfaa322bf13bc62d9c8
shogo <shogo.ohashi0417@gmail.com>
2024-08-17 16:26:12 +0900

    add/収入のクエリサービスを追加

---
+++ app/Infrastructure/Query/Income/IncomeQueryService.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Query\Income;
+
+use App\Models\Income AS IncomeModel;
+use App\Application\Query\Income\IncomeQueryServiceInterface;
+
+final class IncomeQueryService implements IncomeQueryServiceInterface
+{
+    
+}
