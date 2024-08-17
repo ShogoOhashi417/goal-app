666d6c8fc31c8820f05a4a50d31ccc6f67874291
shogo <shogo.ohashi0417@gmail.com>
2024-08-17 16:28:09 +0900

    update/クエリサービスにてモデルを使ってデータを取得する

--- app/Infrastructure/Query/Income/IncomeQueryService.php
+++ app/Infrastructure/Query/Income/IncomeQueryService.php
@@ -9,5 +9,20 @@ use App\Application\Query\Income\IncomeQueryServiceInterface;
 
 final class IncomeQueryService implements IncomeQueryServiceInterface
 {
-    
+    private readonly IncomeModel $incomeModel;
+
+    public function __construct(
+        IncomeModel $incomeModel
+    )
+    {
+        $this->incomeModel = $incomeModel;
+    }
+
+    /**
+     * @return array
+     */
+    public function fetchAll(): array
+    {
+        return $this->incomeModel->fetchAll();
+    }
 }
