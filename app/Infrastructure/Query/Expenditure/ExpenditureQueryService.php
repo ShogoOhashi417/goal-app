1ba8b2d5773abd2704c13ddf35596eeae7f61251
shogo <shogo.ohashi0417@gmail.com>
2024-08-17 17:11:31 +0900

    add/支出情報をすべて取得するメソッド作成

--- app/Infrastructure/Query/Expenditure/ExpenditureQueryService.php
+++ app/Infrastructure/Query/Expenditure/ExpenditureQueryService.php
@@ -9,5 +9,20 @@ use App\Application\Query\Expenditure\ExpenditureQueryServiceInterface;
 
 class ExpenditureQueryService implements ExpenditureQueryServiceInterface
 {
-    
+    private readonly ExpenditureModel $expenditureModel;
+
+    public function __construct(
+        ExpenditureModel $expenditureModel
+    )
+    {
+        $this->expenditureModel = $expenditureModel;
+    }
+
+    /**
+     * @return array
+     */
+    public function fetchAll(): array
+    {
+        return $this->expenditureModel->fetchAll();
+    }
 }
