8d196ebdb264d0be329e3843e50e651b26ebef70
shogo <shogo.ohashi0417@gmail.com>
2024-08-17 17:10:58 +0900

    add/支出情報のクエリサービスを作成

---
+++ app/Infrastructure/Query/Expenditure/ExpenditureQueryService.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Query\Expenditure;
+
+use App\Models\Expenditure AS ExpenditureModel;
+use App\Application\Query\Expenditure\ExpenditureQueryServiceInterface;
+
+class ExpenditureQueryService implements ExpenditureQueryServiceInterface
+{
+    
+}
