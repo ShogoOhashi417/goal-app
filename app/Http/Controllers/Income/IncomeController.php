068c6a4a7bb61d937b85395d5952089bb7ee345c
shogo <shogo.ohashi0417@gmail.com>
2024-08-17 17:17:15 +0900

    update/収入情報を実際のDbから取得する

--- app/Http/Controllers/Income/IncomeController.php
+++ app/Http/Controllers/Income/IncomeController.php
@@ -4,23 +4,26 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Income;
 
+use App\Models\Income AS IncomeModel;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Infrastructure\Query\Income\IncomeQueryService;
+use App\Application\UseCase\Income\Fetch\FetchIncomeUseCase;
 
 class IncomeController extends Controller
 {
     public function index()
     {
-        // todo ダミーデータ
-        $income_info_list = [
-            [
-                'name' => '給料',
-                'amount' => 300000
-            ],
-        ];
+        $fetchIncomeUseCase = new FetchIncomeUseCase(
+            new IncomeQueryService(
+                new IncomeModel()
+            )
+        );
+
+        $incomeInfoList = $fetchIncomeUseCase->handle();
 
         return view('view.income.index', [
-            'income_info_list' => $income_info_list
+            'income_info_list' => $incomeInfoList
         ]);
     }
 }
