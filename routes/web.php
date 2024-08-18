a66cc59ec6bc138b499b8a3c2c3c0fcb57089243
shogo <shogo.ohashi0417@gmail.com>
2024-08-18 13:37:57 +0900

    update/収入追加のルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,8 @@ Route::middleware('auth')->group(function () {
 
 Route::get('/income', 'App\Http\Controllers\Income\IncomeController@index')->name('income');
 
+Route::post('income/add', 'App\Http\Controllers\Income\IncomeController@create');
+
 Route::get('/expenditure', 'App\Http\Controllers\Expenditure\ExpenditureController@index')->name('expenditure');
 
 Route::get('/life_insurance', 'App\Http\Controllers\LifeInsuranceController@index')->name('life_insurance');
