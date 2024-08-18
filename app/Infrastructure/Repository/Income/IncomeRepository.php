2f1022413b759d87644e55f1715f4efa25cddb17
shogo <shogo.ohashi0417@gmail.com>
2024-08-18 10:21:55 +0900

    add/収入のリポジトリを追加

---
+++ app/Infrastructure/Repository/Income/IncomeRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Repository\Income;
+
+use App\Models\Income AS IncomeModel;
+use App\Domain\Model\Income\IncomeRepositoryInterface;
+
+final class IncomeRepository implements IncomeRepositoryInterface
+{
+}
