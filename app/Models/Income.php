1f905b1bb295a206214ee36853483bd0fed0d71f
shogo <shogo.ohashi0417@gmail.com>
2024-08-18 11:22:22 +0900

    update/DBモデルに保存メソッド実装

--- app/Models/Income.php
+++ app/Models/Income.php
@@ -9,11 +9,31 @@ final class Income extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['name', 'amount'];
+
     /**
      * @return array
      */
     public function fetchAll(): array
     {
         return $this->all()->toArray();
+    }
+
+    /**
+     * @param string $name
+     * @param integer $amount
+     * @return void
+     */
+    public function createIncome(
+        string $name,
+        int $amount
+    )
+    {
+        $this->create(
+            [
+                'name' => $name,
+                'amount' => $amount
+            ]
+        );
     }
 }
