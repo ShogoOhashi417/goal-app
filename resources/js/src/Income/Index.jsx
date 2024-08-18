d68b590c000feb3a43af2baf35a81d8af6815d32
shogo <shogo.ohashi0417@gmail.com>
2024-08-18 13:33:21 +0900

    update/収入追加モーダルに入力項目を追加する

--- resources/js/src/Income/Index.jsx
+++ resources/js/src/Income/Index.jsx
@@ -100,6 +100,41 @@ function Income() {
                             </svg>
                         </button>
                     </div>
+                    <div className="grid gap-4 mb-4 grid-cols-2">
+                        <div className="col-span-2">
+                            <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                収入名
+                            </label>
+                            <input
+                                type="text"
+                                name="income_name"
+                                value={incomeName}
+                                onChange={changeIncomeName}
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                placeholder="例) 給料"
+                            />
+                        </div>
+                        <div className="col-span-2">
+                            <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                金額
+                            </label>
+                            <input
+                                type="number"
+                                name="amount"
+                                value={incomeAmount}
+                                onChange={changeIncomeAmount}
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                min="1"
+                            />
+                        </div>
+                    </div>
+                    <button
+                        onClick={addIncome}
+                        type="submit"
+                        className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <svg className="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd"></path></svg>
+                        収入を登録する
+                    </button>
                 </div>
             </div>
         </>
