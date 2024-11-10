ad66962f66d0dde304f78b103e9a95a2970ba2b3
shogo <shogo.ohashi0417@gmail.com>
2024-11-10 11:20:51 +0900

    update/カテゴリーのナビゲーションリンク追加

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -37,6 +37,12 @@ export default function Authenticated({ user, header, children }) {
                                     支出管理
                                 </NavLink>
                             </div>
+
+                            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                <NavLink href={route('category')} active={route().current('category')}>
+                                    カテゴリー
+                                </NavLink>
+                            </div>
                         </div>
 
                         <div className="hidden sm:flex sm:items-center sm:ms-6">
