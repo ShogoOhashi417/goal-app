564dd2497fdcd31946cb810bf9e65dc9b1acfcb3
shogo <shogo.ohashi0417@gmail.com>
2024-11-23 17:47:00 +0900

    update/相対期間での絞り込みを追加

--- resources/js/Components/YearSelectBox.jsx
+++ resources/js/Components/YearSelectBox.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export default function YearSelectBox({onChange}) {
+export default function YearSelectBox({onChange, className = ''}) {
     const currentYear = new Date().getFullYear();
 
     const years = Array.from({ length: 16 }, (_, i) => currentYear - 10 + i);
@@ -19,7 +19,7 @@ export default function YearSelectBox({onChange}) {
         <select
             id="year-select"
             value={selectedYear}
-            className="w-1/6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className={`w-1/6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ` + className}
             onChange={handleChange}
         >
             {years.map((year) => (
