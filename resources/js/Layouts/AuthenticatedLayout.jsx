53a32c1982e82f4e40fc26a60bce3d079f92d6a6
shogo <shogo.ohashi0417@gmail.com>
2025-02-02 16:33:10 +0900

    update/トップ画面にレポートを移動させる

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -21,8 +21,8 @@ export default function Authenticated({ user, header, children }) {
                             </div>
 
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink href={route('dashboard')} active={route().current('dashboard')}>
-                                    Dashboard
+                                <NavLink href={route('report')} active={route().current('report')}>
+                                    トップ
                                 </NavLink>
                             </div>
 
@@ -41,12 +41,6 @@ export default function Authenticated({ user, header, children }) {
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                                 <NavLink href={route('category')} active={route().current('category')}>
                                     カテゴリー
-                                </NavLink>
-                            </div>
-
-                            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink href={route('report')} active={route().current('report')}>
-                                    レポート
                                 </NavLink>
                             </div>
 
