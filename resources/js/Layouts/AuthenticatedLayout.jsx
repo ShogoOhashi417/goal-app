ebae24350bcab9277881bd619c7813fefd36f5be
shogo <shogo.ohashi0417@gmail.com>
2025-02-02 16:40:55 +0900

    update/トップページにレポート画面を移動

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -21,7 +21,7 @@ export default function Authenticated({ user, header, children }) {
                             </div>
 
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink href={route('report')} active={route().current('report')}>
+                                <NavLink href={route('top')} active={route().current('top')}>
                                     トップ
                                 </NavLink>
                             </div>
@@ -116,7 +116,7 @@ export default function Authenticated({ user, header, children }) {
 
                 <div className={(showingNavigationDropdown ? 'block' : 'hidden') + ' sm:hidden'}>
                     <div className="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink href={route('dashboard')} active={route().current('dashboard')}>
+                        <ResponsiveNavLink href={route('top')} active={route().current('top')}>
                             Dashboard
                         </ResponsiveNavLink>
                     </div>
