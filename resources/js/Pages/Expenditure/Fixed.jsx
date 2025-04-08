bcef7ef9b1e112b101d5f5e1a194d53ca2fa16b1
shogo <shogo.ohashi0417@gmail.com>
2025-04-09 07:15:14 +0900

    update/モーダルの終了期間が右寄りだったので左寄りに修正

--- resources/js/Pages/Expenditure/Fixed.jsx
+++ resources/js/Pages/Expenditure/Fixed.jsx
@@ -716,7 +716,7 @@ export default function Fixed({
                                         }
                                         showMonthYearPicker={cycleUnit == 1}
                                         showYearPicker={cycleUnit == 2}
-                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 text-right"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                                         locale={ja}
                                     />
                                 </div>
@@ -1027,7 +1027,7 @@ export default function Fixed({
                                         }
                                         showMonthYearPicker={cycleUnit == 1}
                                         showYearPicker={cycleUnit == 2}
-                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 text-right"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                                         locale={ja}
                                     />
                                 </div>
