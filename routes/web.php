9414a006cd3a1f6619f8ce1771dafed11d70e883
shogo <shogo.ohashi0417@gmail.com>
2025-05-03 11:39:33 +0900

    update/支出レポートの取得方法修正

--- routes/web.php
+++ routes/web.php
@@ -55,6 +55,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/report/saving', [ReportController::class, 'saving'])->name('report.saving');
     Route::get('/report/expense', [ReportController::class, 'expense'])->name('report.expense');
     Route::get('/report/saving/get', [ReportController::class, 'getCategoryToAmountList'])->name('report.saving.get');
+    Route::get('/report/expense/get', [ReportController::class, 'fetchExpenseInfoList'])->name('report.expense.get');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
