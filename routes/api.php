4d36912312cffe823fcc5d21b4e7f9ec584be5cf
shogo <shogo.ohashi0417@gmail.com>
2025-06-02 08:23:02 +0900

    a

--- routes/api.php
+++ routes/api.php
@@ -30,7 +30,14 @@ Route::get('/csrf-token', function () {
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    // SANCTUM_STATEFUL_DOMAINS の値を取得
+    $sanctumStatefulDomains = env('SANCTUM_STATEFUL_DOMAINS');
+
+    // ユーザー情報と環境変数の値を返す
+    return response()->json([
+        'user' => $request->user(),
+        'sanctum_stateful_domains' => $sanctumStatefulDomains,
+    ]);
 });
 
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
