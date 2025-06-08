ad2af8ce20811dcf36dfac6028651d646b3b87ed
shogo <shogo.ohashi0417@gmail.com>
2025-06-09 07:05:15 +0900

    update/不要な記述削除

--- config/app.php
+++ config/app.php
@@ -184,6 +184,4 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
-
-    'db_host' => env('DB_HOST', 'wahaha'),
 ];
